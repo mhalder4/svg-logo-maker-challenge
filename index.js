3080a2cf670e93111252a79006ea97d2c926b044
unknown <mhalder9555@gmail.com>
2023-10-31 21:12:23 -0500

    Added a base logo class amd a square class.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const Square = require("./lib/classes/square");
+
 const questions = [
   {
     type: "input",
@@ -10,12 +12,33 @@ const questions = [
   {
     type: "input",
     message: "What color do you want the text in your logo to be?",
-    name: "color"
+    name: "textColor"
   },
   {
     type: "list",
     message: "What shape do you want your logo to be?",
     choices: ["circle", "triangle", "square"],
     name: "shape"
+  },
+  {
+    type: "input",
+    message: "What color do you want the shape in your logo to be?",
+    name: "shapeColor"
   }
-]
+];
+
+function generateSVG(data) {
+  const square = new Square(data.characters, data.textColor, data.shape, data.shapeColor);
+  console.log(square);
+}
+
+function start() {
+  inquirer
+    .prompt(questions)
+    .then(answers => {
+      console.log(answers);
+      generateSVG(answers);
+    })
+}
+
+start();
