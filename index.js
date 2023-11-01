36ca989c2a56b255b9b6927106d8cbb49d06640f
unknown <mhalder9555@gmail.com>
2023-10-31 23:18:49 -0500

    Finished adding logic to create and write the SVG file for all shapes. Also checks validity of inputs.

--- index.js
+++ index.js
@@ -29,77 +29,118 @@ const questions = [
   }
 ];
 
-function generateSVGText(data) {
-  // const square = new Square(data.characters, data.textColor, data.shape, data.shapeColor);
-  // console.log(square);
+function displayInstructions() {
+  console.log(
+    "\x1b[36m%s\x1b[0m",
+    "This program is intended to create a logo in the shape of either a circle, a triangle, or a square with up to three characters in the middle of the shape. The colors of both the text and the shape can be chosen. If your logo does not appear as anticipated, double check to make sure that there are no typos or incorrect submissions."
+  );
+}
 
-  // const triangle = new Triangle(data.characters, data.textColor, data.shape, data.shapeColor);
-  // console.log(triangle);
+function checkValidity(data) {
+  const charCheck = (data.characters.length > 0) && (data.characters.length < 4);
+  // console.log(charCheck);
+  const textColorCheck = !!data.textColor;
+  // console.log(textColorCheck);
+  const shapeColorCheck = !!data.shapeColor;
+  // console.log(shapeColorCheck);
 
-  // const circle = new Circle(data.characters, data.textColor, data.shape, data.shapeColor);
-  // console.log(circle);
+  if (!charCheck) {
+    console.log("\x1b[31m%s\x1b[0m", "Error: There are either too many or too few characters enterred. Please try again.");
+    return false;
+  } else if (!textColorCheck) {
+    console.log("\x1b[31m%s\x1b[0m", "Error: A text color was not input. Please try again.");
+    return false;
+  } else if (!shapeColorCheck) {
+    console.log("\x1b[31m%s\x1b[0m", "Error: A shape color was not input. Please try again.");
+    return false;
+  } else return true
 
-  let shape;
-  let svgText;
-  switch (data.shape) {
-    case "Circle":
-      shape = new Circle(data.characters, data.textColor, data.shape, data.shapeColor);
-      svgText = generateCircleSVGText(shape);
-      console.log(svgText);
-    case "Triangle":
-      shape = new Triangle(data.characters, data.textColor, data.shape, data.shapeColor);
-      svgText = generateTriangleSVGText(shape);
-      console.log(svgText);
-    case "Square":
-      shape = new Square(data.characters, data.textColor, data.shape, data.shapeColor);
-      svgText = generateSquareSVGText(shape);
-      console.log(svgText);
-  }
 }
 
 function generateCircleSVGText(circle) {
   return `
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
+  <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  
   <circle cx="${circle.cx}" cy="${circle.cy}" r="${circle.r}" fill="${circle.shapeColor}"/>
-
-  <text x="${circle.textSvg.x}" y="${circle.textSvg.y}" font-size="${circle.textSvg.fontSize}" text-anchor="${circle.textSvg.textAnchor}" fill="${circle.textColor}">${circle.chars}</text>
-
-</svg>
+  
+  <text x="${circle.textSvg.x}" y="${circle.textSvg.y}" font-size="${circle.textSvg.fontSize}" text-anchor="${circle.textSvg.textAnchor}" fill="${circle.textColor}" font-family="${circle.textSvg.fontFamily}">${circle.chars}</text>
+  
+  </svg>
   `;
 }
 
 function generateTriangleSVGText(triangle) {
   return `
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
+  <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  
   <polygon points="${triangle.pointA} ${triangle.pointB} ${triangle.pointC}" fill="${triangle.shapeColor}"/>
-
-  <text x="${triangle.textSvg.x}" y="${triangle.textSvg.y}" font-size="${triangle.textSvg.fontSize}" text-anchor="${triangle.textSvg.textAnchor}" fill="${triangle.textColor}">${triangle.chars}</text>
-
-</svg>
+  
+  <text x="${triangle.textSvg.x}" y="${triangle.textSvg.y}" font-size="${triangle.textSvg.fontSize}" text-anchor="${triangle.textSvg.textAnchor}" fill="${triangle.textColor}" font-family="${triangle.textSvg.fontFamily}">${triangle.chars}</text>
+  
+  </svg>
   `;
 }
 
 function generateSquareSVGText(square) {
   return `
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
+  <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  
   <rect x="${square.x}" y="${square.y}" width="${square.width}" height="${square.height}" fill="${square.shapeColor}"/>
-
-  <text x="${square.textSvg.x}" y="${square.textSvg.y}" font-size="${square.textSvg.fontSize}" text-anchor="${square.textSvg.textAnchor}" fill="${square.textColor}">${square.chars}</text>
-
-</svg>
+  
+  <text x="${square.textSvg.x}" y="${square.textSvg.y}" font-size="${square.textSvg.fontSize}" text-anchor="${square.textSvg.textAnchor}" fill="${square.textColor}" font-family="${square.textSvg.fontFamily}">${square.chars}</text>
+  
+  </svg>
   `;
 }
 
+function generateSVGText(data) {
+
+  let shape;
+  let svgText;
+
+  switch (data.shape) {
+    case "Circle":
+      shape = new Circle(data.characters, data.textColor, data.shape, data.shapeColor);
+      svgText = generateCircleSVGText(shape);
+      // console.log(svgText);
+      return svgText;
+    case "Triangle":
+      shape = new Triangle(data.characters, data.textColor, data.shape, data.shapeColor);
+      svgText = generateTriangleSVGText(shape);
+      // console.log(svgText);
+      return svgText;
+    case "Square":
+      shape = new Square(data.characters, data.textColor, data.shape, data.shapeColor);
+      svgText = generateSquareSVGText(shape);
+      // console.log(svgText);
+      return svgText;
+  }
+}
+
+function writeSVGFile(data, text) {
+  const fileName = `${data.characters}_${data.textColor}_${data.shape}_${data.shapeColor}_logo.svg`;
+  fs.writeFile(__dirname + `/./user_logos/${fileName}`, text, (err) =>
+    err ? console.error(err) : console.log(`Generated ${fileName} successfully.`)
+  );
+}
+
+
 function start() {
+  displayInstructions();
+
   inquirer
     .prompt(questions)
     .then(answers => {
       console.log(answers);
-      generateSVGText(answers);
+      if (checkValidity(answers)) {
+        const svgText = generateSVGText(answers);
+        // console.log("Generated a logo.");
+        // console.log(svgText);
+
+        writeSVGFile(answers, svgText);
+      }
     })
 }
 
+// displayInstructions();
 start();
